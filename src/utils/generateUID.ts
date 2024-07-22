53440e9497aa81543fdab58d39cbb75e787bdc36
Sam Haakman <sam@josephmark.studio>
2024-07-22 11:13:46 +1000

    add UID helper

---
+++ src/utils/generateUID.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/a/6248722
+export default function generateUID() {
+  // I generate the UID from two parts here
+  // to ensure the random number provide enough bits.
+  let firstPart: any = (Math.random() * 46656) | 0;
+  let secondPart: any = (Math.random() * 46656) | 0;
+  firstPart = ("000" + firstPart.toString(36)).slice(-3);
+  secondPart = ("000" + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+}
