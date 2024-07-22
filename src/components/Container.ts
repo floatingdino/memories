1ab336212ba70713be675514029ea329b26ab689
Sam Haakman <sam@josephmark.studio>
2024-07-22 10:28:42 +1000

    add basic styles

---
+++ src/components/Container.ts
@@ -0,0 +1,8 @@
+import createStyle from "@josephmark/createstyle"
+
+export const Container = createStyle(
+  "div",
+  "px-2.5 mx-auto max-w-[70ch] w-full"
+)
+
+export default Container
---
+++ src/styles/Type.ts
@@ -0,0 +1,21 @@
+import createStyle from "@josephmark/createstyle"
+
+export const H1 = createStyle(
+  "h1",
+  "text-[1.75rem] leading-[1.2] tracking-[0.013em]" // 28px
+)
+
+export const H2 = createStyle(
+  "h2",
+  "text-[1.375rem] leading-[1.27] tracking-[0.016em]" // 22px
+)
+
+export const H3 = createStyle(
+  "h3",
+  "text-[1.25rem] leading-[1.3] tracking-[0.019em]" // 20px
+)
+
+export const P = createStyle(
+  "p",
+  "text-[1.0625rem] tracking-[-0.04em] leading-[1.5]" // 16px
+)
