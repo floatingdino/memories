9cfe04de90dccd7523ff3a6cff775002b0088635
Sam Haakman <sam@josephmark.studio>
2024-07-22 10:28:09 +1000

    update tooling configs

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: false,
+  bracketSpacing: true,
+  useTabs: false,
+  arrowParens: "always",
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "createStyle"],
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,50 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: [
+        "SF Pro Text",
+        "ui-sans-serif",
+        "system-ui",
+        "Segoe UI",
+        "sans-serif",
+      ],
+      monospace: ["SF Mono", "monospace"],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: "#000",
+        white: "#fff",
+        error: {
+          400: "#FF3B30",
+          500: "#FF453A",
+        },
+        gray: {
+          200: "#F2F2F7",
+          300: "#E5E5EA",
+          400: "#D1D1D6",
+          500: "#C7C7CC",
+          600: "#AEAEB2",
+          700: "#8E8E93",
+        },
+        success: "#12b76a",
+        warning: "#FEC84B",
+        primary: {
+          400: "#007aff",
+          500: "#0a84ff",
+        },
+        "global-bg": "var(--bg-color)",
+      },
+      transitionDelay: {
+        0: "0ms",
+      },
+      transitionProperty: {
+        opacity: "opacity, visibility",
       },
     },
   },
