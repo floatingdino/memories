e81a434e5ea11503d9e414704de798e30e150c9e
Sam Haakman <sam@josephmark.studio>
2024-09-16 17:20:15 +1000

    add task listing page

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/Container"
+import { H1, P } from "@/styles/Type"
+import supabase from "@/utils/supabaseClient"
+
+export default async function TaskList() {
+  const { data: tasks } = await supabase.from("tasks").select(`id, name`)
+
+  return (
+    <Container className="pt-10">
+      <H1 className="mb-5">Select your guesses</H1>
+      <P as="ol" className="list-decimal pb-5 pl-7">
+        {tasks?.map((task) => <li key={task.name}>{task.name}</li>)}
+      </P>
+    </Container>
+  )
+}
