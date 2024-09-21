f1fc1272d320f9bb517693cadea1b8b072f80d68
Sam Haakman <sam@josephmark.studio>
2024-09-21 19:24:23 +1000

    leaderboard improvements

--- src/app/(game)/leaderboard/page.tsx
+++ src/app/(game)/leaderboard/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 
 import Container from "@/components/Container"
-import { H1, H2, H5, P } from "@/styles/Type"
+import { H1, H5 } from "@/styles/Type"
 import supabase from "@/utils/supabaseClient"
 import { GroupPanel } from "../tasks/GroupPanel"
 import { ChangeEventHandler, useCallback, useLayoutEffect, useMemo, useState } from "react"
 import useSWR from "swr"
-import Select from "@/components/Forms/Select"
 import { useSearchParams } from "next/navigation"
 import Field from "@/components/Forms"
 
@@ -51,6 +50,14 @@ const SORT_OPTIONS = ["id", "points", "totalGuesses", "correctGuesses", "incorre
 export default function Leaderboard() {
   const params = useSearchParams()
   const [sort, setSort] = useState(params.get("sort") || "id")
+  const { data: allTasks, mutate: updateTasks } = useSWR("tasks", (table: string) =>
+    supabase
+      .from(table)
+      .select("id, name, guests ( id )")
+      .order("id")
+      .returns<any>()
+      .then(({ data }) => data)
+  )
   const { data: guests, mutate } = useSWR(
     "guests",
     (table: string) =>
@@ -78,21 +85,26 @@ export default function Leaderboard() {
     { refreshWhenHidden: true }
   )
 
-  const enhancedGuests = useMemo(() => {
-    return guests?.map?.((guest) => {
-      const { task, id } = guest
-      if (!task) {
-        return guest
+  const tasks = useMemo(() => {
+    return allTasks?.map((_task) => {
+      const guest = guests?.find(({ id }) => id === _task.guests?.[0]?.id)
+      if (!guest) {
+        return { task: _task, id: _task.id, points: 0, totalGuesses: 0, correctGuesses: 0, incorrectGuesses: 0 }
       }
+      const { task, id } = guest
       const points = task?.goals?.reduce?.((acc, goal) => {
         return acc + goal.points * goal.completions
       }, 0)
-      const guess = task?.guesses?.guess || {}
+      const guess: Record<string, string> = task?.guesses?.guess || {}
 
       const totalGuesses = Object.values(guess).filter(Boolean).length
       const correctGuesses = Object.entries(guess).filter(([guessTask, guessGuest]) => {
-        const taskGuest = guests.find(({ id }) => id === guessGuest)
-        return taskGuest?.task?.[0]?.id === guessTask
+        const taskGuest = guests.find(({ id }) => id.toString() === guessGuest.toString())
+        if (!!guessGuest) {
+          console.log(guests)
+          console.log(taskGuest, guessGuest, guessTask)
+        }
+        return taskGuest?.task?.id?.toString() === guessTask
       })
 
       return {
@@ -103,7 +115,7 @@ export default function Leaderboard() {
         incorrectGuesses: totalGuesses - correctGuesses.length,
       }
     })
-  }, [guests])
+  }, [guests, allTasks])
 
   useLayoutEffect(() => {
     const channels = [
@@ -111,6 +123,7 @@ export default function Leaderboard() {
         .channel("guests")
         .on("postgres_changes", { event: "INSERT", schema: "public", table: "guests" }, () => {
           mutate()
+          updateTasks()
         })
         .subscribe(),
       supabase
@@ -136,28 +149,89 @@ export default function Leaderboard() {
     setSort(e.currentTarget.value)
   }, [])
 
-  const sortedGuests = useMemo(() => enhancedGuests?.sort?.((a, b) => b[sort] - a[sort]), [enhancedGuests, sort])
+  const sortedGuests = useMemo(() => tasks?.sort?.((a, b) => b[sort] - a[sort]), [tasks, sort])
+
+  const orderedGuessesByProminence = useMemo(() => {
+    const tasks: Record<number, number> = Object.fromEntries(allTasks?.map((task) => [task.id, 0]) || [])
+    guests?.forEach((guest) => {
+      const guess = guest.task?.guesses?.guess || {}
+      Object.entries(guess)
+        .filter(([_k, v]) => !!v)
+        .forEach(([taskId]) => {
+          tasks[taskId]++
+        })
+    })
+
+    return Object.entries(tasks).sort((a, b) => b[1] - a[1])
+  }, [tasks, guests])
 
   return (
     <Container>
       <div className="py-10">
-        <div className="mb-2">
-          <H1>Leaderboard</H1>
-        </div>
-        <Field
-          onChange={onChange}
-          value={sort}
-          name="sort"
-          type="select"
-          label="Sort by:"
-          options={SORT_OPTIONS.map((value) => ({ label: value, value }))}
-          className="mb-5"
-        />
-        <div className="flex flex-col gap-2">
-          {sortedGuests?.map((guest) => (
-            <LeaderboardGuest showName={!!params.get("showName")} key={guest.id} guest={guest} />
-          ))}
-        </div>
+        {orderedGuessesByProminence.length > 0 && (
+          <>
+            <H5 className="mb-2">Most guessed tasks:</H5>
+            <GroupPanel className="mb-5">
+              {orderedGuessesByProminence.slice(0, 3).map(([id, count]) => {
+                const task = allTasks.find((task) => task.id === parseInt(id))
+                if (!task) {
+                  return null
+                }
+                return (
+                  <div key={id} className="flex">
+                    <H5>{task.name}</H5>
+                    <div className="grow" />
+                    <H5>&times; {count}</H5>
+                  </div>
+                )
+              })}
+            </GroupPanel>
+          </>
+        )}
+        {orderedGuessesByProminence.length > 0 && (
+          <>
+            <H5 className="mb-2">Least guessed tasks:</H5>
+            <GroupPanel className="mb-5">
+              {[...orderedGuessesByProminence]
+                .reverse()
+                .slice(0, 3)
+                .map(([id, count]) => {
+                  const task = allTasks.find((task) => task.id === parseInt(id))
+                  if (!task) {
+                    return null
+                  }
+                  return (
+                    <div key={id} className="flex">
+                      <H5>{task.name}</H5>
+                      <div className="grow" />
+                      <H5>&times; {count}</H5>
+                    </div>
+                  )
+                })}
+            </GroupPanel>
+          </>
+        )}
+        {!!params.get("showLeaderboard") && (
+          <>
+            <div className="mb-2">
+              <H1>Leaderboard</H1>
+            </div>
+            <Field
+              onChange={onChange}
+              value={sort}
+              name="sort"
+              type="select"
+              label="Sort by:"
+              options={SORT_OPTIONS.map((value) => ({ label: value, value }))}
+              className="mb-5"
+            />
+            <div className="flex flex-col gap-2">
+              {sortedGuests?.map((guest) => (
+                <LeaderboardGuest showName={!!params.get("showName")} key={guest.id} guest={guest} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </Container>
   )
